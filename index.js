3a9564f0f75ebc816de3ee0f93152c14b8e47c7e
Steven Pardo <dspardo6@gmail.com>
2019-06-11 18:29:43 -0500

    Update ES6

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-var netanos = require('./Netanos.js');
+const netanos = require('./Netanos.js');
+const xlsx = require('xlsx');
 
-var xlsx = require('xlsx');
-
-var wb = xlsx.readFile('relatos.xlsx');
-
-var ws = wb.Sheets['relatos'];
-
-var data = xlsx.utils.sheet_to_json(ws);
+const wb = xlsx.readFile('./docs/relatos.xlsx');
 console.log(wb.SheetNames);
-console.log(data);
 
-var newData = data.map(function(record) {
-    record.relatoAn = netanos.noncontext(record.relato);
-    delete record.relato
+const ws = wb.Sheets['relatos'];
+
+const data = xlsx.utils.sheet_to_json(ws);
+
+data.map((record) => {
+  record.relatoAn = netanos.noncontext(record.relato);
+  delete record.relato;
 });
 
-var newWB = xlsx.utils.book_new();
-var newWs = xlsx.utils.json_to_sheet(data);
+const newWB = xlsx.utils.book_new();
+const newWs = xlsx.utils.json_to_sheet(data);
+
 xlsx.utils.book_append_sheet(newWB, newWs, "New Data");
 
-xlsx.writeFile(newWB, 'NuevoRelatos.xlsx');
+xlsx.writeFile(newWB, './docs/relatos-anonymized.xlsx');
