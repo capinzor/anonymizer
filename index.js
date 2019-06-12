57b7dd27edb1afd69b1519a2673efb7da10955ce
Steven Pardo <dspardo6@gmail.com>
2019-06-11 19:10:17 -0500

    Versioning out file

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const netanos = require('./Netanos.js');
 const xlsx = require('xlsx');
 
-const wb = xlsx.readFile('./docs/relatos.xlsx');
-console.log(wb.SheetNames);
+const path = './docs/relatos.xlsx';
+const column = 'relato2';
+const sheet = 1;
 
-const ws = wb.Sheets['relatos'];
+const newName = (path) => {
+  path = path.split('/');
+  let [name, extension] = path.pop().split('.');
+  return `${path.join('/')}/${name}_${Date.now()}.anonymized.${extension}`;
+}
+
+const wb = xlsx.readFile(path);
+
+const ws = wb.Sheets[wb.SheetNames[sheet - 1]];
 
 const data = xlsx.utils.sheet_to_json(ws);
 
-data.map((record) => {
-  record.relatoAn = netanos.noncontext(record.relato);
-  delete record.relato;
-});
+data.map(record => record[column] = netanos.noncontext(record[column]));
 
 const newWB = xlsx.utils.book_new();
 const newWs = xlsx.utils.json_to_sheet(data);
 
-xlsx.utils.book_append_sheet(newWB, newWs, "New Data");
+xlsx.utils.book_append_sheet(newWB, newWs, 'New Data');
 
-xlsx.writeFile(newWB, './docs/relatos-anonymized.xlsx');
+xlsx.writeFile(newWB, newName(path));
